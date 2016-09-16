fb054f83a28e002e4c21bd70eeffa23420cf7fce
dbo <kxnmei@163.com>
2016-09-16 22:18:06 +0800

    添加 for、while特殊格式及function 练习脚本

--- learning_bash/算术.sh
+++ learning_bash/算术.sh
@@ -158,4 +158,254 @@ done
 echo "Add $users users."
 
 
+=========
+even.sh
+=========
+
+#!/bin/bash
+#
+
+declare -i i=0
+declare -i sum=0
+
+until [ $i -gt 100 ]; do
+    let i++
+    if [ $[$i%2] -eq 1 ]; then
+        continue
+    fi
+    let sum+=$i
+done
+
+echo "Even sum: $sum"
+
+
+
+===========
+user_log.sh
+===========
+#!/bin/bash
+#
+
+read -p "Enter a user name: " username
+
+while true; do
+    if who | grep "^$username" &> /dev/null; then
+        break
+    fi
+    sleep 3
+done
+echo "$username logged on." >> /tmp/user.log
+
+
+
+# 改写
+#!/bin/bash
+#
+
+read -p "Enter a user name: " username
+while ! who | grep "^$username" &> /dev/null; do
+        break
+    sleep 3
+done
+echo "$username logged on." >> /tmp/user.log
+
+# 改写
+#!/bin/bash
+#
+
+read -p "Enter a user name: " username
+until who | grep "^$username" &> /dev/null; do
+        break
+    sleep 3
+done
+echo "$username logged on." >> /tmp/user.log
+
+
+=========
+evenid.sh
+=========
+#!/bin/bash
+
+while read line; do
+    if [ $[$(echo $line | cut -d: -f3) % 2] -eq 0 ]; then
+        echo -e -n "username: $(echo $line | cut -d: -f1)\t"
+        echo "uid: $(echo $line | cut -d: -f3)"
+    fi
+done < /etc/passwd
+
+
+========
+sum2.sh
+========
+# for 的特殊格式
+#!/bin/bash
+#
+declare -i sum=0
+
+for ((i=1;i<=100;i++)); do
+    let sum+=$i
+done
+
+echo "Sum: $sum."
+
+# 9X9乘法表
+#!/bin/bash
+#
+
+for ((j=1;j<=9;j++)); do
+    for ((i=1;i<=j;i++)); do
+        echo -e -n "${i}X${j}=$[$i*$j]\t"
+    done
+    echo
+done
+
+
+==========
+sysinfo.sh
+==========
+#!/bin/bash
+#
+cat << EOF
+cpu) show cpu information;
+mem) show memory information;
+disk) show disk information;
+quit) quit
+============================
+EOF
+
+read -p "Enter a option: " option
+while [ "$option" != 'cpu' -a "$option" != 'mem' -a "$option" != 'disk' -a "$option" != 'quit' ]; do
+    read -p "Wrong option,Enter again: " option
+done
+
+if [ "$option" == 'cpu' ]; then
+    lscpu
+elif [ "$option" == 'mem' ]; then
+    cat /proc/meminfo
+elif [ "$option" == 'disk' ]; then
+    fdisk -l
+else
+    echo "Quit"
+    exit 0
+fi
+
+
+# case 实现
+#!/bin/bash
+#
+cat << EOF
+cpu) show cpu information;
+mem) show memory information;
+disk) show disk information;
+quit) quit
+============================
+EOF
+
+read -p "Enter a option: " option
+while [ "$option" != 'cpu' -a "$option" != 'mem' -a "$option" != 'disk' -a "$option" != 'quit' ]; do
+    read -p "Wrong option,Enter again: " option
+done
+
+case "$option" in
+cpu)
+    lscpu
+    ;;
+mem)
+    cat /proc/meminfo
+    ;;
+disk)
+    fdisk -l
+    ;;
+*)
+    echo "Quit..."
+    exit 0
+esac
+
+
+=============
+function 练习
+=============
+#!/bin/bash
+#
+username='myuser'
+function adduser {
+    if id $username &> /dev/null; then
+        echo "$username exists."
+        return 1
+    else
+        useradd $username
+        [ $? -eq 0 ] && echo "Add $username finished." && return 0
+    fi
+}
+
+adduser
+
+
+===========
+服务脚本
+===========
+#!/bin/bash
+#
+# chkconfig: - 88 12
+# description: test service script
+#
+prog=$(basename $0)
+lockfile=/var/lock/subsys/$prog
+
+start() {
+    if [ -f $lockfile ]; then
+        echo "$prog is already running."
+        return 0
+    else
+        touch $lockfile
+        [ $? -eq 0 ] && echo "Starting $prog finished."
+    fi
+}
+
+stop() {
+    if [ -f $lockfile ]; then
+        rm -f $lockfile && echo "Stop $prog ok."
+    else
+        echo "$prog is stopped yet."
+    fi
+}
+
+status() {
+    if [ -f $lockfile ]; then
+        echo "$prog is running."
+    else
+        echo "$prog is stoped."
+    fi
+}
+
+usage(){
+    echo "Usage: $prog {start|stop|restart|status}"
+}
+
+if [ $# -lt 1 ]; then
+    usage
+    exit 1
+fi
+
+case $1 in
+start)
+    start
+    ;;
+stop)
+    stop
+    ;;
+restart)
+    stop
+    start
+    ;;
+status)
+    status
+    ;;
+*)
+    usage
+    ;;
+esac
+
+
+
 
